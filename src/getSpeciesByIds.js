f4fb7211f5889e0ff0cb7f18a142cdc424b4d65b
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-09 20:13:17 -0300

    Finalizando requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  //  seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const filteredSpecies = data.species.filter((species) => ids.includes(species.id));
+  return filteredSpecies;
 };
 
 module.exports = getSpeciesByIds;
+
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
+getSpeciesByIds(['0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46']);
