9f04785f711a2855845b4b781654738561656c66
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-10 06:19:45 -0300

    requisitos 1 ao 7 completos

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('retorna quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  test('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  test('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  test('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  test('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  test('retorna undefined caso não entregue nenhum parâmetro', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+});
