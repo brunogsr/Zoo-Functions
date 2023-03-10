c6225ca88df3867adaac46c812e84e71e19abcbf
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-09 22:04:53 -0300

    finalizando etapa 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const employee = data.employees.find((employees) =>
+    (employees.firstName === employeeName || employees.lastName === employeeName));
+  return employee;
 };
+
+console.log(getEmployeeByName('Wishart'));
+// console.log(getEmployeeByName('Elser'));
 
 module.exports = getEmployeeByName;
