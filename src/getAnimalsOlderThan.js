982fdf6b3f3b1b7a8e002e3c7bbc6f4d2a5f2186
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-09 21:24:25 -0300

    requisito 2 completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+const getAnimalsOlderThan = (animal, newAge) => {
+  const animalCheck = data.species.find((species) => species.name === animal);
+  const ageDataSpecies = animalCheck.residents.every((residents) => residents.age >= newAge);
+  return ageDataSpecies;
 };
 
+getAnimalsOlderThan('otters');
+// [10, 12, 2, 2]
+
+// const ageCheck = animalCheck.filter((animalCheck) => animalCheck.newAge < animalCheck.age);
+// console.log(ageCheck)
 module.exports = getAnimalsOlderThan;
