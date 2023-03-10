48460a67f1fb5d0fcbf204a3d099fbfc378415c7
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-10 05:48:02 -0300

    Requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (animals) => {
+  const speciesObj = data.species;
+  // const selectedAnimal = data.species.find((species) => species.name === animal);
+  if (!animals) {
+    const animais = speciesObj.reduce((accumulator, especie) => {
+      accumulator[especie.name] = especie.residents.length;
+      return accumulator;
+    }, {});
+    return animais;
+  }
+  const selectedAnimal = speciesObj.find((animaisTotais) =>
+    animaisTotais.name === animals.species);
+  const animalsWithSex = selectedAnimal.residents.filter((element) =>
+    element.sex === animals.sex);
+  if (!animals.sex) {
+    return selectedAnimal.residents.length;
+  }
+  console.log(animalsWithSex.length);
+  return animalsWithSex.length;
 };
+countAnimals({ species: 'giraffes', sex: 'female' });
 
 module.exports = countAnimals;
