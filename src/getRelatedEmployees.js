cb211e60aa582888a88f0bcc5382fb14cd2496c8
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-10 02:28:22 -0300

    requisito 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const managerCheck = data.employees.some((employee) => employee.managers.includes(id));
+  console.log(managerCheck);
+  return managerCheck;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const managedIds = data.employees.filter((managedEmployee) => managedEmployee.managers
+      .includes(managerId));
+    return managedIds.map((managedEmployee) =>
+      `${managedEmployee.firstName} ${managedEmployee.lastName}`);
+  }
 };
 
 module.exports = { isManager, getRelatedEmployees };
+
+// coletar todos os firstName e Lastname das pessoas com o "managerID" nas chaves "managers:"
+// armazená-los em um array
+// return com a concatenação de cada um repetidamente
